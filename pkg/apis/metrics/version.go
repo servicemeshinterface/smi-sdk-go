fab4e131337c69953d35fdd3b4953f3063848539
grampelberg <thomas@saunter.org>
2019-05-06 09:53:25 -0700

    Move to v1alpha1

--- pkg/apis/metrics/version.go
+++ pkg/apis/metrics/version.go
@@ -4,7 +4,7 @@ import (
 	"path"
 )
 
-const APIVersion = "metrics.smi-spec.io/v1beta1"
+const APIVersion = "metrics.smi-spec.io/v1alpha1"
 
 func baseURL() string {
 	return path.Join("/apis", APIVersion)
