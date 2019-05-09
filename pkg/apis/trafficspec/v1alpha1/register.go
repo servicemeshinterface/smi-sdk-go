31dde7317b2bc599114ffda648b3c371ca1fa928
Nicholas Jackson <jackson.nic@gmail.com>
2019-05-09 05:45:42 +0100

    rename register

--- pkg/apis/trafficspec/v1alpha1/register.go
+++ pkg/apis/trafficspec/v1alpha1/register.go
@@ -40,7 +40,7 @@ var (
 // Adds the list of known types to Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
-		&HTTPRoute{},
+		&HTTPRouteGroup{},
 		&TCPRoute{},
 		&TrafficTarget{},
 		&IdentityBinding{},
