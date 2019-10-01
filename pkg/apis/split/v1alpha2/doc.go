70439895a7cfa724b59269892dad18dac082f17b
Michelle Noorali <michellemolu@gmail.com>
2019-10-01 18:23:51 -0400

    update weight to int

--- pkg/apis/split/v1alpha2/doc.go
+++ pkg/apis/split/v1alpha2/doc.go
@@ -1,4 +1,4 @@
 // +k8s:deepcopy-gen=package
 // +groupName=split.smi-spec.io
 
-package v1alpha1
+package v1alpha2
--- pkg/gen/client/split/clientset/versioned/scheme/register.go
+++ pkg/gen/client/split/clientset/versioned/scheme/register.go
@@ -17,7 +17,7 @@ limitations under the License.
 package scheme
 
 import (
-	splitv1alpha1 "github.com/deislabs/smi-sdk-go/pkg/apis/split/v1alpha1"
+	splitv1alpha2 "github.com/deislabs/smi-sdk-go/pkg/apis/split/v1alpha2"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
@@ -29,7 +29,7 @@ var Scheme = runtime.NewScheme()
 var Codecs = serializer.NewCodecFactory(Scheme)
 var ParameterCodec = runtime.NewParameterCodec(Scheme)
 var localSchemeBuilder = runtime.SchemeBuilder{
-	splitv1alpha1.AddToScheme,
+	splitv1alpha2.AddToScheme,
 }
 
 // AddToScheme adds all types of this clientset into the given scheme. This allows composition
