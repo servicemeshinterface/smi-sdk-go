114b7c5ddf132c747149eb99f0919690f58dc245
stefanprodan <stefan.prodan@gmail.com>
2020-03-20 19:58:50 +0200

    build: refactor codegen script - fix: #12
    Signed-off-by: stefanprodan <stefan.prodan@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -2,51 +2,43 @@
 
 set -eu
 
-# ROOT_PACKAGE :: the package (relative to $GOPATH/src) that is the target for code generation
 ROOT_PACKAGE="github.com/servicemeshinterface/smi-sdk-go"
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+
+# get code-generator version from go.sum
+CODEGEN_VERSION="$(grep 'k8s.io/code-generator' go.sum | awk '{print $2}' | head -1)"
+CODEGEN_PKG="$(echo `go env GOPATH`"/pkg/mod/k8s.io/code-generator@${CODEGEN_VERSION}")"
+
+# ensure we can execute the codegen script
+chmod +x ${CODEGEN_PKG}/generate-groups.sh
 
 function generate_client() {
-
-  SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  ROOT_DIR="$( cd $SCRIPT_ROOT/.. && pwd )"
-  GEN_VER=$( awk '/k8s.io\/code-generator/ { print $2 }' "$ROOT_DIR/go.mod" )
-  CODEGEN_PKG=${GOPATH}/pkg/mod/k8s.io/code-generator@${GEN_VER}
-
-  # CUSTOM_RESOURCE_NAME :: the name of the custom resource that we're generating client code for
   CUSTOM_RESOURCE_NAME=$1
   CUSTOM_RESOURCE_VERSIONS=$2
 
-  # make the generate script executable
-  chmod +x ${CODEGEN_PKG}/generate-groups.sh
-
-  # delete the generated code as this is additive, removed objects will not be cleaned
-
-  # enumerate versions
+  # delete the generated deepcopy
   for V in ${CUSTOM_RESOURCE_VERSIONS//,/ }; do
     rm -f ${ROOT_DIR}/pkg/apis/${CUSTOM_RESOURCE_NAME}/${V}/zz_generated.deepcopy.go
   done
+
+  # delete the generated code as this is additive, removed objects will not be cleaned
   rm -rf ${ROOT_DIR}/pkg/gen/client/${CUSTOM_RESOURCE_NAME}
 
+  # the generate-groups.sh script cannot handle group names with dashes, so we use smispec.io as the group name
   if [[ "$OSTYPE" == "darwin"* ]]; then
     find "${ROOT_DIR}/pkg/apis/${CUSTOM_RESOURCE_NAME}" -type f -exec sed -i '' 's/smi-spec.io/smispec.io/g' {} +
   else
     find "${ROOT_DIR}/pkg/apis/${CUSTOM_RESOURCE_NAME}" -type f -exec sed -i 's/smi-spec.io/smispec.io/g' {} +
   fi
 
-  # generate the code with:
-  # --output-base    because this script should also be able to run inside the vendor dir of
-  #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-  #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-  GO111MODULE="on" "${CODEGEN_PKG}"/generate-groups.sh \
-    "deepcopy,client,informer,lister" \
+  # run code generation
+  "${CODEGEN_PKG}"/generate-groups.sh all \
     "$ROOT_PACKAGE/pkg/gen/client/$CUSTOM_RESOURCE_NAME" \
     "$ROOT_PACKAGE/pkg/apis" \
     $CUSTOM_RESOURCE_NAME:$CUSTOM_RESOURCE_VERSIONS \
-    --go-header-file "${SCRIPT_ROOT}"/boilerplate.go.txt
+    --go-header-file "${ROOT_DIR}"/hack/boilerplate.go.txt
 
-  # The generate-groups.sh script cannot handle group names with dashes, so we use
-  # smispec.io as the group name, then replace it with smi-spec.io after code
-  # generation.
+  # replace smispec.io with smi-spec.io after code generation
   if [[ "$OSTYPE" == "darwin"* ]]; then
     find "${ROOT_DIR}/pkg/apis/${CUSTOM_RESOURCE_NAME}" -type f -exec sed -i '' 's/smispec.io/smi-spec.io/g' {} +
     find "${ROOT_DIR}/pkg/gen/client/${CUSTOM_RESOURCE_NAME}" -type f -exec sed -i '' 's/smispec.io/smi-spec.io/g' {} +
