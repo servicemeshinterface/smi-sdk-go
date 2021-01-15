7d0e0a4d58bf6824f920907b50498d5b8564e762
Nic Jackson <jackon.nic@gmail.com>
2021-01-15 20:37:18 +0000

    Start adding conversion code
    Signed-off-by: Nic Jackson <jackson.nic@gmail.com>

---
+++ pkg/apis/access/v1alpha2/webhook.go
@@ -0,0 +1,13 @@
+package v1alpha2
+
+import "sigs.k8s.io/controller-runtime/pkg/conversion"
+
+func (src *TrafficTarget) ConvertTo(dstRaw conversion.Hub) error {
+
+	return nil
+}
+
+func (src *TrafficTarget) ConvertFrom(srcRaw conversion.Hub) error {
+
+	return nil
+}
