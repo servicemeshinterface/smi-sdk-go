96986737d5ef6671b06b480e4935512e8a97a736
Nic Jackson <jackon.nic@gmail.com>
2021-01-16 10:01:29 +0000

    remove imports
    Signed-off-by: Nic Jackson <jackson.nic@gmail.com>

--- pkg/apis/access/v1alpha2/webhook.go
+++ pkg/apis/access/v1alpha2/webhook.go
@@ -1,13 +1,4 @@
 package v1alpha2
 
-import "sigs.k8s.io/controller-runtime/pkg/conversion"
-
-func (src *TrafficTarget) ConvertTo(dstRaw conversion.Hub) error {
-
-	return nil
-}
-
-func (src *TrafficTarget) ConvertFrom(srcRaw conversion.Hub) error {
-
-	return nil
-}
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+func (*TrafficTarget) Hub() {}
